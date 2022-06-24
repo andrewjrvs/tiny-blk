b438789ac1268b54680c9694bfb18b953677a3a3
Andrew Jarvis <6360004+andrewjrvs@users.noreply.github.com>
2022-06-24 22:49:44 +0000

    update angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { GameService } from 'projects/tiny-blk/src/public-api';
 
 @Component({
@@ -6,16 +6,19 @@ import { GameService } from 'projects/tiny-blk/src/public-api';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
 
   title = 'tiny-blk';
   constructor(private gmSrv: GameService) {
 
   }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
   @ViewChild('dropBlock') canvas: ElementRef;
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     // const cv = document.getElementById('dropBlock');
     const cv = this.canvas.nativeElement;
     this.gmSrv.start(cv as HTMLCanvasElement);
